97d6e3ec3b56a068100d3c2b5ee61b19edbdc80f
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2019-01-06 20:46:18 +0300

    Added Model file for sepet_urun

---
+++ app/Models/SepetUrun.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SepetUrun extends Model
+{
+    Use SoftDeletes;
+
+    protected $table = "sepet_urun";
+    protected $guarded = [];
+
+    const CREATED_AT = 'olusturulma_tarihi';
+    const UPDATED_AT = 'guncellenme_tarihi';
+    const DELETED_AT = 'silinme_tarihi';
+
+}
