4c68115b654f71eca5888a87b884fbb35d40a1cc
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2019-01-06 20:17:41 +0300

    Added model file for sepet

---
+++ app/Models/Sepet.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Sepet extends Model
+{
+    //
+    use SoftDeletes;
+
+    protected $table = 'sepet';
+
+    protected $guarded = [];
+
+    const CREATED_AT = 'olusturulma_tarihi';
+    const UPDATED_AT = 'guncellenme_tarihi';
+    const DELETED_AT = 'silinme_tarihi';
+}
