ab736b5f958dcd54df7b01b57d34da21f93c32e6
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2019-01-12 19:53:43 +0300

    Added siparis table

---
+++ database/migrations/2019_01_12_164942_create_siparis_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSiparisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siparis', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sepet_id')->unsigned();
+            $table->decimal('siparis_tutari', 5, 4);
+            $table->string('durum', 30)->nullable();
+            $table->string('banka', 30)->nullable();
+
+            $table->timestamp('olusturulma_tarihi')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('guncellenme_tarihi')->default(DB::raw('CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('silinme_tarihi')->nullable();
+
+            $table->unique('sepet_id');
+            $table->foreign('sepet_id')->references('id')->on('sepet')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siparis');
+    }
+}
