373252748d3ecf67a7a3393dabc25a32b005dc44
Yiğit ALTINTAŞ <yigitaltintas@outlook.com.tr>
2019-01-12 19:57:40 +0300

    Added sipars model

--- app/Models/Siparis.php
+++ app/Models/Siparis.php
@@ -5,20 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Sepet extends Model
+class Siparis extends Model
 {
     //
     use SoftDeletes;
 
-    protected $table = 'sepet';
+    protected $table = "siparis";
 
-    protected $guarded = [];
+    protected $fillable = [
+        'sepet_id',
+        'siparis_tutari',
+        'banka',
+        'taksit_sayisi',
+        'durum'
+    ];
 
     const CREATED_AT = 'olusturulma_tarihi';
     const UPDATED_AT = 'guncellenme_tarihi';
     const DELETED_AT = 'silinme_tarihi';
 
-    public function siparis(){
-        return $this->hasOne('App\Models\Siparis');
+    public function sepet(){
+        return $this->belongsTo('App\Models\Sepet');
     }
 }
